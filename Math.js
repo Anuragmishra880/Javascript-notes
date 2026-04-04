a8dcf7a2cd46f53a6481f770eb7732bfbd76fc89
Anuragmishra880 <anuragmishra9821497411@gmail.com>
2026-04-04 15:05:18 +0530

    Regularly used Math functions of JavaScript

---
+++ Math.js
@@ -0,0 +1,14 @@
+Math.round(10.5) = 11
+Math.ceil(10.1) = 11
+Math.floor(12.8) = 12
+Math.trunc(18.9) = 18
+Math.pow(2, 3) = 8
+Math.sqrt(25) = 5
+Math.cbrt(8) = 2
+Math.abs(-12) = 12
+Math.abs(12) = 12
+Math.max(25, 41, 12) = 410
+Math.min(35, 41, 12) = 12
+Math.fround(8) = 8
+let a = 85.3313
+console.log(a.toFixed(3)) = 85.331
